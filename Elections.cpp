c4eed411c8d97a8168faa5eff390510ed8362895
Badsha012 <badshasheikh361@gmail.com>
2026-01-05 22:13:25 +0600

    nine commit

---
+++ Elections.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        long long A = max(0LL, max(b, c) + 1 - a);
+        long long B = max(0LL, max(a, c) + 1 - b);
+        long long C = max(0LL, max(a, b) + 1 - c);
+
+        cout << A << " " << B << " " << C << "\n";
+    }
+    return 0;
+}
