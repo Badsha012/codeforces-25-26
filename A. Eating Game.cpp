8f62d137111f2dd6d6929ac098af984d6282f089
Badsha012 <badshasheikh361@gmail.com>
2026-02-27 20:52:09 +0600

    B. Deletion Sort

--- A. Eating Game.cpp
+++ A. Eating Game.cpp
@@ -5,35 +5,50 @@
 
 using namespace std;
 
-void solve() {
+int solve() {
     int n;
-    cin >> n;
+    if (!(cin >> n)) return 0;
     vector<int> a(n);
-    long long total_dishes = 0;
-    for (int i = 0; i < n; i++) {
+    int total_dishes = 0;
+    for (int i = 0; i < n; ++i) {
         cin >> a[i];
         total_dishes += a[i];
     }
 
-    set<int> winners;
+    set<int> possible_winners;
 
-    
-    for (int start_player = 0; start_player < n; start_player++) {
-       
-        int winner_idx = (start_player + (int)((total_dishes - 1) % n)) % n;
-        winners.insert(winner_idx);
+   
+    for (int start_player = 0; start_player < n; ++start_player) {
+        vector<int> temp_a = a;
+        int dishes_left = total_dishes;
+        int current_player = start_player;
+        int last_winner = -1;
+
+        while (dishes_left > 0) {
+            if (temp_a[current_player] > 0) {
+                temp_a[current_player]--;
+                dishes_left--;
+                if (dishes_left == 0) {
+                    last_winner = current_player + 1; // Store 1-based index
+                }
+            }
+           
+            current_player = (current_player + 1) % n;
+        }
+        possible_winners.insert(last_winner);
     }
 
-    cout << winners.size() << endl;
+    return possible_winners.size();
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--) {
-        solve();
+        cout << solve() << "\n";
     }
     return 0;
 }
