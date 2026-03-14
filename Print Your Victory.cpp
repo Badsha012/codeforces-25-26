da9b5c6f38caaa065371dddee3ab990b4489453a
Badsha012 <badshasheikh361@gmail.com>
2026-03-14 20:59:11 +0600

    added a toph problem

---
+++ Print Your Victory.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Standard I/O optimization (optional for this simple task)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Print the required string exactly
+    cout << "Red light. Green light. Accepted." << endl;
+
+    return 0;
+}
